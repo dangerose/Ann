8547107b373fef02ebc47fea38cf5876edd7d220
chenyun <chenyun@landicorp.com>
2019-06-18 21:43:43 +0800

    图片 - 点击放大功能

---
+++ static/js/index.js
@@ -0,0 +1,55 @@
+/* 图片放大插件 */
+(function() {
+    function picZoom () {
+
+    }
+
+    picZoom.prototype.show = function(url) {
+        this._clear();
+        this._add(url);
+    }
+    picZoom.prototype.hide = function() {
+        this._clear();
+    }
+
+    // 清除
+    picZoom.prototype._clear = function() {
+        var $picZooms = $('body').find('.zoom');
+        $picZooms.remove();
+    }
+    // 添加
+    picZoom.prototype._add = function(url) {
+        var self = this;
+        var $body = $('body');
+        var $newZoom = $('\
+            <div class="zoom">\
+                <div class="zoom-overlay"></div>\
+                <div class="zoom-content d-n" id="zoomContent">\
+                    <img class="zoom-pic" src="../static/img/001.jpg"/>\
+                </div>\
+                <div class="zoom-load" id="zoomLoad">\
+                    <img class="zoom-loadicon" src="../static/img/loading.gif"/>\
+                </div>\
+            </div>\
+        ');
+        $newZoom.on('click', function() {
+            self.hide();
+        });
+        $newZoom.find('.zoom-pic').attr('onload', function() {
+            $newZoom.find('.zoom-content').removeClass('d-n');
+            $newZoom.find('.zoom-load').addClass('d-n');
+        });
+        $body.append($newZoom);
+    }
+
+    window.$picZoom = new picZoom();
+})();
+
+(function() {
+    // 图片点击事件
+    var $content = $('#content');
+    $content.delegate('img', 'click', function() {
+        var $img = $(this);
+        $picZoom.show($img.attr('src'));
+    });
+})()
