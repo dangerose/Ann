34c9efd7ac03d5f3dbb41d6597aa10eaab62d15c
zhengda.xu <zhengda.xu@baishancloud.com>
2019-06-29 00:34:22 +0800

    Fix: template and static path

--- server/server.py
+++ server/server.py
@@ -10,12 +10,16 @@ define("port", default=8080, help="running on given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render(os.path.join(os.getcwd(), '../views/index.html'))
+        self.render('index.html')
 
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", MainHandler)])
+    app = tornado.web.Application(
+            handlers=[(r"/", MainHandler)],
+            template_path=os.path.join(os.path.dirname(__file__), "../views"),
+            static_path=os.path.join(os.path.dirname(__file__), "../static")
+        )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
