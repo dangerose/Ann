24d2b570220282213e4e8725dc7c6e40adc2a31f
zhengda.xu <zhengda.xu@baishancloud.com>
2019-07-05 23:28:22 +0800

    Fix: debug

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@ import tornado.options
 import tornado.web
 
 from tornado.options import options, define
+from PIL import Image
 
 define("port", default=8080, help="running on given port", type=int)
 define("username", default="admin", help="user name", type=str)
@@ -60,45 +61,96 @@ class LogoutHandler(BaseHandler):
 
 class UploadHandler(BaseHandler):
     def post(self, *args, **kwargs):
+        print('posting')
         menuId = self.get_argument('menuId', '/')
         picId = self.get_argument('picId', 'hello')
         file_imgs = self.request.files['img']
         menu_path = '../static/img/{0}'.format(menuId)
         save_to = '../static/img/{0}/{1}'.format(menuId, picId)
+        return_status = {
+            "status": None,
+            "msg": None
+        }
+        print(menu_path)
+        print(save_to)
         try:
             if not os.path.exists(menu_path):
                 os.makedirs(menu_path)
 
             for file_img in file_imgs:
                 with open(save_to, 'wb') as f:
-                    print('open ok')
                     f.write(file_img["body"])
-                    print('write ok')
-                    self.finish({"status":"ok",
-                                  "msg": "操作成功"})
-                    print('upload img success')
+                    return_status["status"] = "ok"
+                    return_status["msg"] = "success"
 
         except Exception as e:
-            self.finish({"status": "fail",
-                         "msg": str(e)}) 
+            return_status["status"] = "fail"
+            return_status["msg"] = str(e)
             print(e)
+
+        finally:
+            self.finish(return_status)
                     
 
 
 class DeleteHandler(BaseHandler):
-    def post(self):
-        pass
-        
-class GetPathHandler(BaseHandler):
     def get(self, *args, **kwargs):
         menuId = self.get_argument('menuId', None)
         picId = self.get_argument('picId', None)
 
         file_path = '../static/img/{0}/{1}'.format(menuId, picId)
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        else:
-            pass
+        return_status = {
+            "status": None,
+            "msg": None
+        }
+        try:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                return_status["status"] = "ok"
+                return_status["msg"] = "success"
+                print("delete ok")
+            else:
+                return_status["status"] = "fail"
+                return_status["msg"] = file_path + " not found"
+                print("delete not found")
+
+        except Exception as e:
+            return_status["status"] = "fail"
+            return_status["msg"] = str(e)
+            print(str(e))
+
+        finally:
+            self.finish(return_status)
+        
+
+class GetPathHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        menuId = self.get_argument('menuId', None)
+        menu_path = '../static/img/{0}/'.format(menuId)
+
+        files = os.listdir(menu_path)
+        return_data = {
+            "status": None,
+            "msg": None,
+            "data": []
+        }
+        for f in files:
+            if not os.path.isdir(f):
+                print('deleting file %s', f)
+                file_path = menu_path.format(f)
+                img = Image.open(file_path)
+                img_size = img.size
+                min_size = min(img_size)
+                max_size = max(img_size)
+                if os.path.splitext(f)[1]=='.jpg' or os.path.splitext(f)[1]=='.png':
+                    data = {}
+                    picId = os.path.splitext(f)[0]
+                    size = "{0}*{1}".format(str(max_size), str(min_size)) 
+                    data['picId'] = picId
+                    data['size'] = size
+                    return_data['data'].append(data)
+        
+        self.finish(return_data)
 
 
 
